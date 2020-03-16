960d18d484ebd5fccaa1e812d3779347cd339fe4
wulinyun <lin.wu@landasoft.com>
2020-03-16 18:47:16 +0800

    京东开放平台sdk使用初始化项目代码

---
+++ src/test/java/com/sagesoft/jd/openapidemo/OpenApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagesoft.jd.openapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
